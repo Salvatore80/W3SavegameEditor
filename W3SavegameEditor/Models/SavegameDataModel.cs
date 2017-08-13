19b53c94fdfb5ac5401c4a33048db95cefb20fe7
Salvatore80 <Salvatore80@users.noreply.github.com>
2017-08-13 17:48:03 +0200

    Show number of saves and loads

--- W3SavegameEditor/Models/SavegameDataModel.cs
+++ W3SavegameEditor/Models/SavegameDataModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace W3SavegameEditor.Models
 {
@@ -11,5 +12,21 @@ namespace W3SavegameEditor.Models
         public ObservableCollection<string> VariableNames { get; set; }
 
         public ObservableCollection<VariableModel> Variables { get; set; }
+
+        public int Saves
+        {
+            get
+            {
+                return Variables.Single(v => v.Name == "saveInfo").Children.Where(v => v.Name == "type").Count(v => v.Value == "2");
+            }
+        }
+
+        public int Loads
+        {
+            get
+            {
+                return Variables.Single(v => v.Name == "saveInfo").Children.Where(v => v.Name == "type").Count(v => v.Value == "3");
+            }
+        }
     }
 }
